e94d54f25ecbcae965ce401198904e504144b5a0
Lukas <lukasz.gryc@yahoo.com>
2024-06-15 20:22:37 +0200

    Added hw depended libraries needed for test compilation

---
+++ UnitTest/include/config/test_config.h
@@ -0,0 +1,14 @@
+/*
+ * test_config.h
+ *
+ *  Created on: 09.06.2024
+ *      Author: Boss
+ */
+
+#ifndef TEST_CONFIG_H_
+#define TEST_CONFIG_H_
+
+#define __AVR_ATmega328P__
+#define F_CPU 2000000UL
+
+#endif /* TEST_CONFIG_H_ */
